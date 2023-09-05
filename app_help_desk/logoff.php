ae8c00957426705f5d28cde90862e8a465788d1c
Arthur Gabriel Silva Martins <90343022+ArthurGSMartins@users.noreply.github.com>
2023-09-05 11:47:47 -0300

    Add files via upload

---
+++ app_help_desk/logoff.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+/*
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+unset($_SESSION['x']);
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+*/
+
+session_destroy();
+header('Location: index.php')
+
+
+/*
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+*/
+?>
